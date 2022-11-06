fd99908ede8a3de061fabf5064c850e47935ec24
Sirine-Chi <71520044+Sirine-Chi@users.noreply.github.com>
2022-11-06 14:56:14 +0400

    Update Start.py

--- Start.py
+++ Start.py
@@ -53,7 +53,7 @@ def format_objects(index, c_file):
         obj_vals.append(line_vals[8]) #colour
 
         objs.append(obj_vals)
-        print(obj_vals)
+        #print(obj_vals)
     return objs
 
 mode = find_value_by_name('Mode', config)
@@ -69,22 +69,27 @@ pulse_table = bool(int( find_value_by_name('Pulse_table', config) ))
 
 objects = format_objects('obj_', config)
 
+print(objects) #ПЕЧАТАТЬ С РАЗДЕЛИТЕЛЕМ \n
 if mode == 'Simulation':
-    print('mode =', mode)
+    print('mode =', mode, '  method =', method)
     delta_cur = 0
     inum = 's'
     print('N =',N,'  time_direction =',time_direction,'  end_time =',end_time,'  time_step =',time_step,'  delta_cur =',delta_cur,'  inum =',inum,'  pulse_table =',pulse_table)
-    nbl.simul(objects, N, time_direction, end_time, time_step, delta_cur, inum, pulse_table) #+force_function
+    nbl.simul(method, objects, N, time_direction, end_time, time_step, delta_cur, inum, pulse_table) #+force_function
 
 if mode == 'Progons':
-    print('mode = ', mode)
+    print('mode =', mode, '  method =', method)
 
-    progons_per_delta = 1
-    delta_r = 0
-    nbl.progons(dur, end, dt, delta_step, k, delta_start, delta_end, pulse_table) #+force_function
+    progons_per_delta = int( find_value_by_name('Progons_per_delta', config) )
+    delta_start = float( find_value_by_name('Delta_start', config) )
+    delta_end = float( find_value_by_name('Delta_end', config) )
+    delta_step = float( find_value_by_name('Delta_step', config) )
+
+    #КАКИЕ ФЙЛЫ СОХРАНИЛИСЬ? СОЗДАВАТЬ НОВУЮ ДИРЕКТОРИЮ ПО ВРЕМЕНИ ЗАПУСКА ПРОГОНОВ
+    print('N =',N,'  time_direction =',time_direction,'  end_time =',end_time,'  time_step =',time_step,'  pulse_table =',pulse_table)
+    print('Delta step =',delta_step,'  Delta start =',delta_start,'  Delta end =',delta_end,'Progons per delta =',progons_per_delta)
+    nbl.progons(method, objects, N, time_direction, end_time, time_step, delta_step, progons_per_delta, delta_start, delta_end, pulse_table) #+force_function
 
 # if mode == 'Field':
 #     print('mode = ', mode)
-    #nbl.field_simulation(force_function, N, end_time, time_step, list_of_objects, )
-
-
+    #nbl.field_simul(method, objects, N, time_direction, end_time, time_step, delta_cur, inum, pulse_table)
