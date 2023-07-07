c1c3115b0625af7504a161ebd922e3658d98cdf6
Sirine-Chi <71520044+Sirine-Chi@users.noreply.github.com>
2023-07-07 15:57:05 +0300

    Prototype of Simulator Class was maden.

--- nbody/simulator.py
+++ nbody/simulator.py
@@ -1,7 +1,62 @@
 import NBodyLib as nbl
+import time
+import numpy as np
+import random2
+
+def v(lisst):
+    return np.array(lisst)
+
+# Типо много тел на прямой с разными координатами
 
 class Simulator():
-    def __init__(self):
+    def __init__(self, r, v, m, time, step, *args, **kwargs):
+        self.m = m
+        self.time = time
+        self.step = step
+        self.delta_time = 0
+
+        self.ts = []
+        self.rs = []
+        self.vs = []
+        self.ts.append(0)
+        self.rs.append(r)
+        self.vs.append(v)
+
         print("Simulator Initialized!")
 
-Simulator()
+    def iteration(self, step):
+        self.vs.append(self.vs[-1])
+        self.rs.append(self.rs[-1] + step*self.vs[-1]/(self.m**2))
+        # print(self.rs)
+
+    def render_simulation(self, total_time, step):
+        start_time = time.time()
+        tau = step
+        while tau < total_time:
+            Simulator.iteration(self, step)
+            self.ts.append(tau)
+            tau += step
+        self.delta_time = time.time() - start_time
+
+
+    def get_time(self):
+        return self.delta_time
+    
+    def get_plot_data(self):
+        return self.rs[-1]
+
+N = 10 # Bodies
+r_s = []
+v_s = []
+m_s = []
+for i in range(1, N, 1):
+    r_s.append(random2.uniform(-10, 10))
+    v_s.append(random2.uniform(-2, 2))
+    m_s.append(random2.uniform(0, 6))
+print('R', r_s, '\n', 'V', v_s, '\n', 'M', m_s)
+total_time = 100
+step = 0.1
+
+s = Simulator(v(r_s), v(v_s), v(m_s), time, step)
+s.render_simulation(total_time, step)
+print(s.get_plot_data())
