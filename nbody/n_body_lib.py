23b1c6b1ea62f1a15be149f6d09bce0d3368de6b
Sirine-Chi <71520044+Sirine-Chi@users.noreply.github.com>
2023-08-03 22:14:12 +0300

    Added docstrings to n_body_lib Renamed Reader to data_manager

--- nbody/n_body_lib.py
+++ nbody/n_body_lib.py
@@ -4,33 +4,68 @@ from numpy import array as v
 import pandas as pd
 import math
 import random2 as rnd
-import time
+from time import monotonic
 import datetime
 import pyopencl as cl
+from numba import jit, prange
 from tqdm import tqdm
 from typing import Final
-
 import colorama
+from colorama import Fore, Back, Style
 colorama.just_fix_windows_console()
 colorama.init()
-from colorama import Fore, Back, Style
 
-from numba import jit, prange
+def scal(vec: np.ndarray) -> float:  # Lenth of the vector
+    """
+    Vector length in Euclidian space (l2 norm, or scalar)
+    \n
+    v: np.ndarray | vector
+    return: float | scalar
+    """
+    return np.linalg.norm(vec, ord=2)
 
-def scal(v: np.ndarray) -> float:  # Lenth of the vector
-    return np.linalg.norm(v, ord=2)
-def unvec(v: np.ndarray) -> np.ndarray:  # unit vector
-    return v / scal(v)
-def ranvec(r: float) -> np.ndarray:  # Random vector with lengh r
+
+def unvec(vec: np.ndarray) -> np.ndarray:  # unit vector
+    """
+    Vector with same direction as on input, but with length = 1 (unit-vector)
+    \n
+    vec: np.ndarray | vector
+    return: np.ndarray | unit-vector
+    """
+    return vec / scal(v)
+
+
+def ranvec(r: float) -> np.ndarray:
+    """
+    Random vector with lengh r
+    \n
+    r: float | vector length
+    return: np.ndarray | random vector
+    """
     rv = v([rnd.uniform(-r, r), 2 * (rnd.getrandbits(1) - 0.5) * (r ** 2 - rnd.uniform(-r, r) ** 2) ** 0.5])
     return rv
-def ranrv(r: float) -> np.ndarray:  # Random vector with random radius < r
+
+
+def ranrv(r: float) -> np.ndarray:
+    """
+    Random vector with random radius < r
+    \n
+    r: float | length
+    return: np.ndarray | random vector
+    """
     a = rnd.uniform(0, 2 * math.pi)
     rr = rnd.uniform(0, r)
     rv = v([rr * math.cos(a), rr * math.sin(a)])  # radians
     return rv
+
+
 def rotvec(vec: np.ndarray, al: float) -> np.ndarray:
-    # rotates 2D vector on AL degrees, multiplies rotation matrix on vector   v' = M*v
+    """Rotates 2D vector on AL degrees, multiplies rotation matrix on vector   v' = M*v
+    \n
+    vec: np.ndarray | vector to rotate
+    al: float | rotation angle in degrees
+    returns: np.ndarray | rotated vector
+    """
     rotation_mx = v([[math.cos(math.radians(al)), math.sin(math.radians(al))],
                      [-1 * math.sin(math.radians(al)), math.cos(math.radians(al))]])
     return np.matmul(vec, rotation_mx)
