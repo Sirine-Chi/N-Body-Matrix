e3ba975f6eb560848db9450cdd115516dad52b05
Kamil Biktimirov <Biktimirov.kamil@yandex.ru>
2023-08-06 00:34:58 +0300

    Formatted controller.py file

--- nbody/controller.py
+++ nbody/controller.py
@@ -1,8 +1,12 @@
 import time
 
-import n_body_lib as nbl
-import numpy as np
-import random2
+import nbody.n_body_lib as nbl
+
+import random
+
+# FIXME: I have no idea where this module comes from,
+# so it should be gotten from somewhere or
+# there's gonna be an error otherwise ¯\_(ツ)_/¯
 from simulator_test import Simulator
 
 # === === Set variables === ===
@@ -16,10 +20,12 @@ step = 0.1
 r_s = []
 v_s = []
 m_s = []
+
 for i in range(1, N, 1):
-    r_s.append(random2.uniform(-10, 10))
-    v_s.append(random2.uniform(-2, 2))
-    m_s.append(random2.uniform(0, 6))
+    r_s.append(random.uniform(-10, 10))
+    v_s.append(random.uniform(-2, 2))
+    m_s.append(random.uniform(0, 6))
+
 print("R", r_s, "\n", "V", v_s, "\n", "M", m_s)
 
 # === === Actual Run === ===
