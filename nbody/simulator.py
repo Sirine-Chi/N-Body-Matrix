33742999e73a2d2eeb192c3770890e5bf1261891
Sirine-Chi <71520044+Sirine-Chi@users.noreply.github.com>
2025-03-17 11:06:10 -0400

    Now better, still problems

--- nbody/simulator.py
+++ nbody/simulator.py
@@ -1,65 +1,85 @@
-# from dataclasses import dataclass as component
+from dataclasses import dataclass as component
+from random import randrange
 import esper
 import mylinal as l
 import mymath
-from loguru import logger
+# import inspect
+# from loguru import logger
 
-logger.add(lambda msg: print(msg, end=""), level="TRACE") # FIXME move to main / entry point
-is_logs: bool = True
-if is_logs == True:
-    logger.add("dev/logs/trace_{time}.log", level="TRACE")
+# logger.add(lambda msg: print(msg, end=""), level="TRACE")
+# is_logs: bool = True
+# if is_logs == True:
+#     logger.add("dev/logs/trace_{time}.log", level="TRACE")
+
+def euler(x_n, y_n, f: callable, h: float):
+    """Euler's nm
+
+    Args:
+        x_n (MATH): previous X
+        y_n (MATH): previous Y
+        f (callable): function f(x_n, y_n)
+        h (float): step dx
+
+    Returns:
+        MATH: returns x_n type
+    """
+    return x_n + h*f(x_n, y_n)/2
 
 # --- --- --- --- --- COMPONENTS
 
-# @component
-class Mass(float):
-    pass
-    # mass: float
+@component
+class Name:
+    mame: str
+
+@component
+class Mass:
+    # pass
+    mass: float
     # def __init__(self, mass: float) -> None:
     #     self.mass = mass
 
-# @component
+@component
 class Position:
-    positions: list[l.Array] = [l.Array.cartesian_array([0.0, 0.0, 0.0])]
-    def __init__(self, position: l.Array.cartesian_array) -> None:
-        self.positions.append(position)
+    # pass
+    positions: list[l.Array] # = [l.Array.cartesian_array([0.0, 0.0, 0.0])]
+    # def __init__(self, position: l.Array = l.Array.cartesian_array([0.0, 0.0, 0.0]) ) -> None:
+    #     super().append(position)
+    def __str__(self):
+        return self.positions.__str__()
 
-# @component
+
+@component
 class Velocity:
-    velocities: list[l.Array] = [l.Array.cartesian_array([0.0, 0.0, 0.0])]
-    def __init__(self, velocity: l.Array.cartesian_array) -> None:
-        self.velocities.append(velocity)
+    # pass
+    velocities: list[l.Array] # = [l.Array.cartesian_array([0.0, 0.0, 0.0])]
+    # def __init__(self, velocity: l.Array) -> None:
+    #     super().append(velocity)
 
-# @component
+@component
 class Acceleration:
-    accelerations: list[l.Array] = [l.Array.cartesian_array([0.0, 0.0, 0.0])]
-    def __init__(self, acceleration: l.Array.cartesian_array) -> None:
-        self.accelerations.append(acceleration)
+    # pass
+    accelerations: list[l.Array] # = [l.Array.cartesian_array([0.0, 0.0, 0.0])]
 
-# # @component
-# class Force:
-#     force: l.Array
-#     def __init__(self, force: l.Array) -> None:
-#         self.force = force
+    # def __str__(self):
+    #     return self.accelerations.__str__()
+    # def __init__(self, acceleration: l.Array) -> None:
+    #     super().append(acceleration)
 
-# @component
-class ForceSelfOnOthers:
-    isactingonothers: bool
-
-# @component
-class ForceOthersOnSelf:
-    isothersacting: bool
+@component
+class Force:
     force: l.Array
-    def set_force(self, force: l.Array) -> None:
-        self.force = force
+
+    def __str__(self):
+        return self.force.__str__()
 
 # @component
 class UpdatesAnalytically:
     isupdatesanalytically: bool
 
-# @component
+@component
 class Visualised:
-    isvisualised: bool
+    # pass
+    # isvisualised: bool
     color: str
 
 # @component
@@ -72,7 +92,8 @@ class Particle:
 
 class ForceHandler:
 
-    def gravityforce_1(self, r1:l.Array, r2:l.Array, m1:Mass, m2:Mass) -> l.Array:
+    @staticmethod
+    def gravityforce_1(r1:l.Array, r2:l.Array, m1:Mass, m2:Mass) -> l.Array:
         """
         Calculate the gravitational force between two particles.
 
@@ -88,23 +109,44 @@ class ForceHandler:
 
         return mymath.G * m1 * m2 * (r1 - r2) / ((r1 - r2).l.scal())**3
 
-    def gravityforce_2(self, p1, p2) -> l.Array:
-        """
-        Calculate the gravitational force between two particles.
-
-        Args:
-            r1 (l.Array): Position vector of the first particle.
-            r2 (l.Array): Position vector of the second particle.
-            m1 (Mass): Mass of the first particle.
-            m2 (Mass): Mass of the second particle.
-
-        Returns:
-            l.Array: Gravitational force vector acting on the first particle due to the second particle.
-        """
-        r1, m1 = esper.get_components(p1, Position, Mass)
-        r2, m2 = esper.get_components(p2, Position, Mass)
+    @staticmethod
+    def gravity_force_ent(ent1: int, ent2: int):
+        m1 = esper.try_component(ent1, Mass)
+        m2 = esper.try_component(ent2, Mass)
+        r1 = esper.try_component(ent1, Position)[-1]
+        r2 = esper.try_component(ent1, Position)[-1]
 
         return mymath.G * m1 * m2 * (r1 - r2) / ((r1 - r2).l.scal())**3
+
+    @staticmethod
+    def hooke_force(r1: l.Array, r2:l.Array):
+        st_lenght = 1.0
+        return mymath.K * ( (r1 - r2)/(r1 - r2).l.scal() - st_lenght)
+
+    @staticmethod
+    def hooke_force_ent(ent1: int, ent2: int):
+        r1 = esper.try_component(ent1, Position)[-1]
+        r2 = esper.try_component(ent2, Position)[-1]
+        st_lenght = 1.0
+        return 0 * ( (r1 - r2)/(r1 - r2).l.scal() - st_lenght)
+
+    # def gravityforce_2(self, p1, p2) -> l.Array:
+    #     """
+    #     Calculate the gravitational force between two particles.
+
+    #     Args:
+    #         r1 (l.Array): Position vector of the first particle.
+    #         r2 (l.Array): Position vector of the second particle.
+    #         m1 (Mass): Mass of the first particle.
+    #         m2 (Mass): Mass of the second particle.
+
+    #     Returns:
+    #         l.Array: Gravitational force vector acting on the first particle due to the second particle.
+    #     """
+    #     r1, m1 = esper.get_components(p1, Position, Mass)
+    #     r2, m2 = esper.get_components(p2, Position, Mass)
+
+    #     return mymath.G * m1 * m2 * (r1 - r2) / ((r1 - r2).l.scal())**3
 
     # def coulombforce(self, p1, p2) -> l.Array:
     #     r1, q1 = esper.get_components(p1, Position, Charge)
@@ -121,20 +163,35 @@ class ForceProcessor(esper.Processor):
     Args:
         esper (_type_): _description_
     """
+    all_funcs = {
+            "1" : ForceHandler.gravity_force_ent,
+            "2": ForceHandler.hooke_force_ent
+            }
+
     def __init__(self, timestep: float = 1.0):
         self. timestep = timestep
 
     def process(self):
-        for ent2, (pos2, mass2) in esper.get_components(Position, Mass, ForceSelfOnOthers): # here we get entities, with ForceSelfOnOthers
-            for ent1, (pos1, mass1) in esper.get_components(Position, Mass, ForceOthersOnSelf):
-                if ent1 != ent2:
-                    print("type: " + type(ent1)) # FIXME delete after testing
-                    force = ForceHandler().gravityforce_1(pos1[-1], pos2[-1], mass1, mass2)
-                    
+        # for ent2, (pos2, mass2) in esper.get_components(Position, Mass): # here we get entities, with ForceSelfOnOthers
+        #     for ent1, (pos1, mass1) in esper.get_components(Position, Mass):
+        #         if ent1 != ent2:
+        #             force = ForceHandler().gravityforce_1(pos1[-1], pos2[-1], mass1, mass2)
 
+        for force_id, force in self.all_funcs.items():
+            for ent2 in esper.get_component(Force):
+                for ent1, (f1) in esper.get_component(Force):
+                    if ent1 != ent2:
+                        if isinstance(ent1, int) and isinstance(force_id, str): # если тело2 действует на
+                            if isinstance(ent2, int) and isinstance(force_id, str): # если действуют на тело 1
+                                f1 += force(ent1, ent2)
+                                # esper.component_for_entity(ent1, Force) # = force(ent1, ent2)
 
-                    
-
+        for ent, (frc, acc, m) in esper.get_components(Force, Acceleration, Mass):
+            acc.append(frc/m)
+        
+        for ent, (acc, vel, pos) in esper.get_components(Acceleration, Velocity, Position):
+            vel.append(vel[-1] + acc*self.timestep)
+            pos.append(pos[-1] + vel*self.timestep)
                     
 
 # class ForceCollectingProcessor(esper.Processor):
@@ -160,34 +217,75 @@ class AnalyticCoordinateUpdateProcessor(esper.Processor):
  
 class VisualProcessor(esper.Processor):
     def process(self) -> None:
+        vis_positions = []
         for ent, (pos, vis) in esper.get_components(Position, Visualised):
-            pass
+            vis_positions.append(esper.try_component(ent, pos[-1]))
+        
+        print(f"all positions: {vis_positions}")
             # call to visualiser
 
 # --- --- --- --- --- ENT INITIALISATION
 
-for i in range(1, 100):
+for i in range(1, 10):
     particle = esper.create_entity()
 
+    pp = l.Array.cartesian_array([randrange(1, 10), randrange(1, 10), randrange(1, 10)])
+    pv = l.Array.cartesian_array([randrange(1, 10), randrange(1, 10), randrange(1, 10)])
+    # pa = l.Array.cartesian_array([randrange(1, 10), randrange(1, 10), randrange(1, 10)])
 
-    esper.add_component(particle, Position)
-    esper.add_component(particle, Velocity)
+    esper.add_component(particle, Name(f"Particle n. {particle}"))
+    esper.add_component(particle, Mass(randrange(1, 10)))
+    esper.add_component(particle, Position([pp]))
+    esper.add_component(particle, Velocity([pv]))
+    esper.add_component(particle, Force(l.Array([0.0, 0.0, 0.0]))) # FIXME dimensional independent INIT'ion
+    esper.add_component(particle, Visualised)
+
+                            # esper.component_for_entity(ent1, Force) # = force(ent1, ent2)
+                            # print("force:" + force(ent1, ent2))
+                            # print(f"Force: {esper.try_component(particle, Force)}")
+    # esper.add_component(particle, Acceleration(esper.try_component(particle, Force) / esper.try_component(particle, Mass)))
+
+    print(f"INIT: {esper.try_components(particle, Name, Mass, Position)}")
+
+# --- --- --- --- --- FIRST FORCE COLLECTION
+
+for force_id, force in ForceProcessor.all_funcs.items():
+    for ent2 in esper.get_component(Force):
+        for ent1, (f1) in esper.get_component(Force):
+            if ent1 != ent2:
+                if isinstance(ent1, int) and isinstance(force_id, str): # если тело2 действует на
+                    if isinstance(ent2, int) and isinstance(force_id, str): # если действуют на тело 1
+                        print(f"f1:: {f1}")
+                        f1 += force(ent1, ent2)
+
+# for ent in esper.get_component(Force):
+#     esper.add_component(ent, Acceleration)
+
+for ent, (frc, acc, m) in esper.get_components(Force, Acceleration, Mass):
+    acc.append(frc/m)
 
 # --- --- --- --- --- PROCESSORS INITIALISATION
 
+t = 0
+t_end = 1
+step = 0.1
+
 # forcecollectingprocessor = ForceCollectingProcessor()
 # forceapplictionprocessor = ForceApplicationProcessor()
-forceprocessor = ForceProcessor()
+forceprocessor = ForceProcessor(timestep=step)
 # analyticcoordinateupdateprocessor = AnalyticCoordinateUpdateProcessor()
-# visualprocessor = VisualProcessor()
+visualprocessor = VisualProcessor()
 
 # esper.add_processor(forcecollectingprocessor)
 # esper.add_processor(forceapplictionprocessor)
-esper.add_processor(forceprocessor)
+esper.add_processor(forceprocessor, priority=10)
 # esper.add_processor(analyticcoordinateupdateprocessor)
-# esper.add_processor(visualprocessor)
+esper.add_processor(visualprocessor, priority=1)
 
 # --- --- --- --- --- PROCESSING
 
-esper.process()
+while t < t_end:
+    esper.process()
+    # print(f"t={t}, {esper.try_component(1, Position)}")
+    t += step
 # print(esper.list_worlds())
