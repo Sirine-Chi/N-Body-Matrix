f56d98063ad346d08ab759893fefb1bec2fcccc4
Sirine-Chi <71520044+Sirine-Chi@users.noreply.github.com>
2025-03-19 09:29:37 -0400

    Added decrorator init Method feature, now method init is consistent!

--- nbody/numerical.py
+++ nbody/numerical.py
@@ -1,22 +1,10 @@
 import inspect
-# def euler_improved(xs, ys, f:callable, h: float):
-#     """
-#     Euler's Improved nm
+def euler_improved(xs, ys, f:callable, h: float):
+    requirement_depth = 1
 
-#     Args:
-#         x_n (MATH): previous X
-#         y_n (MATH): previous Y
-#         f (callable): function f(x_n, y_n)
-#         h (float): step dx
-
-#     Returns:
-#         MATH: returns x_n type
-#     """
-#     requirement_depth = 1
-
-#     k1 = h*f(xs[-1], ys[-1])
-#     k2 = h*f(xs[-1]+h, ys[-1]+k1)
-#     return ys[-1] + (k1+k2)/2
+    k1 = h*f(xs[-1], ys[-1])
+    k2 = h*f(xs[-1]+h, ys[-1]+k1)
+    return ys[-1] + (k1+k2)/2
 
 # def two_step(x_n, y_n, y_n_, f:callable, h: float):
 #     """
@@ -57,13 +45,22 @@ class Method:
         else:
             raise IndexError(f"Method require depth {self.order},but len xs={len(self.xs)}, len ys={len(self.ys)} < {self.order}")
 
+def new_method(order: int):
+    def dec(func: callable):
+        return Method(order, func)
+    return dec
 
-def euler_f(xs, ys, f: callable, h: float):
+# --- --- --- METHOD OBJECT CREATION
+
+@new_method(order=1)
+def euler(xs, ys, f: callable, h: float):
     return xs[-1] + h*f(xs[-1], ys[-1])/2
 
-euler = Method(order=1, func=euler_f)
+# --- --- --- USAGE
 
 def exf(x, y):
     return x + y
 
-print( f"\033[1mMethod instance __str__: \033[0m \n{euler}\n\033[1mMethod: \033[0m {euler(xs=[0], ys=[2], f=exf, h=0.1)}, \033[1m \nFunc: \033[0m {euler_f(xs=[0], ys=[2], f=exf, h=0.1)}" )
+print(f"euler_obj: {euler}, euler_ars: {euler(xs=[0], ys=[2], f=exf, h=0.1 )}")
+
+# print( f"\033[1mMethod instance __str__: \033[0m \n{euler}\n\033[1mMethod: \033[0m {euler(xs=[0], ys=[2], f=exf, h=0.1)}, \033[1m \nFunc: \033[0m {euler_f(xs=[0], ys=[2], f=exf, h=0.1)}" )
