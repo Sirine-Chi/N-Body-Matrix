23593281b55f15ee8a0fb44ecf218c619d7c2af7
Sirine-Chi <71520044+Sirine-Chi@users.noreply.github.com>
2025-03-19 09:45:15 -0400

    doc'ed

--- nbody/numerical.py
+++ nbody/numerical.py
@@ -1,29 +1,11 @@
+"""
+Numerical methods module
+
+Has class, which contains order checking logic and string representations
+Has some methods, new methods can be declared there
+"""
+
 import inspect
-def euler_improved(xs, ys, f:callable, h: float):
-    requirement_depth = 1
-
-    k1 = h*f(xs[-1], ys[-1])
-    k2 = h*f(xs[-1]+h, ys[-1]+k1)
-    return ys[-1] + (k1+k2)/2
-
-# def two_step(x_n, y_n, y_n_, f:callable, h: float):
-#     """
-#     Euler's Improved nm
-
-#     Args:
-#         x_n (MATH): previous X
-#         y_n (MATH): previous Y
-#         y_n_ (MATH): previous previous Y
-#         f (callable): function f(x_n, y_n)
-#         h (float): step dx
-
-#     Returns:
-#         MATH: returns x_n type
-#     """
-#     requirement_depth = 2
-#     return y_n_ + 2*h*f(x_n, y_n)
-
-# each method has requirement_depth, to check, if there's enough values
 
 class Method:
     def __init__(self, order: int, func: callable):
@@ -51,16 +33,62 @@ def new_method(order: int):
     return dec
 
 # --- --- --- METHOD OBJECT CREATION
+# to add new method, just add @new_method(order:int) decorator =)
 
 @new_method(order=1)
-def euler(xs, ys, f: callable, h: float):
+def euler(xs:list, ys:list, f: callable, h: float):
+    """_summary_
+
+    Args:
+        xs (list): _description_
+        ys (list): _description_
+        f (callable): _description_
+        h (float): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return xs[-1] + h*f(xs[-1], ys[-1])/2
 
-# --- --- --- USAGE
+@new_method(order=1)
+def euler_improved(xs:list, ys:list, f:callable, h: float):
+    """
+    (Euler)[https://en.wikipedia.org/wiki/Euler_method]
 
-def exf(x, y):
-    return x + y
+    Args:
+        x_n (MATH): previous Xs list
+        y_n (MATH): previous Ys list
+        f (callable): function f(x_n, y_n)
+        h (float): step dx
 
-print(f"euler_obj: {euler}, euler_ars: {euler(xs=[0], ys=[2], f=exf, h=0.1 )}")
+    Returns:
+        MATH: returns x_n type
+    """
+    k1 = h*f(xs[-1], ys[-1])
+    k2 = h*f(xs[-1]+h, ys[-1]+k1)
+    return ys[-1] + (k1+k2)/2
+
+@new_method(order=2)
+def two_step(xs: list, ys: list, f:callable, h: float):
+    """
+    (Two step nm)[]
+
+    Args:
+        x_n (MATH): previous Xs list
+        y_n (MATH): previous Ys list
+        f (callable): function f(x_n, y_n)
+        h (float): step dx
+
+    Returns:
+        MATH: returns x_n type
+    """
+    return ys[-2] + 2*h*f(xs[-1], ys[-1])
+
+# --- --- --- TESTING
+
+# def exf(x, y):
+#     return x + y
+
+# print(f"euler_obj: {euler}, euler_ars: {euler(xs=[0], ys=[2], f=exf, h=0.1 )}")
 
 # print( f"\033[1mMethod instance __str__: \033[0m \n{euler}\n\033[1mMethod: \033[0m {euler(xs=[0], ys=[2], f=exf, h=0.1)}, \033[1m \nFunc: \033[0m {euler_f(xs=[0], ys=[2], f=exf, h=0.1)}" )
